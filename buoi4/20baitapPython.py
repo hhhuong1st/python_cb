400f227a5010a063ba3a363a88e173fd5b1a2ebb
huong <huynhhuyenhuong2006@gmailcom>
2025-11-24 16:06:40 +0700

    update

--- buoi4/20baitapPython.py
+++ buoi4/20baitapPython.py
@@ -6,12 +6,14 @@ while True:
     except ValueError:
         print ("Lỗi kiểu dữ liệu")
 
+###########################
+
 print("== Câu 1 ==")
 for i in range (1, n+1):
     if i%2==0:
         print(i,end=" ")
 
-
+############################
 
 print("== Câu 2 ==")
 tong = 0
@@ -20,14 +22,14 @@ for i in range(1,n+1):
         tong +=i
 print(f" Tổng các số lẻ từ 1 đến N là: {tong}")
 
-
+############################
 
 print("== Câu 3 ==")
 for i in range(1,n):
         if i%3 ==0:
             print(i, end =" ")
 
-
+############################
 
 print("== Câu 4 ==")
 i=1
@@ -42,13 +44,13 @@ while(i<=10):
         print("Lỗi kiểu dữ liệu")
 print (f"Có {dem} số âm")
 
-
+############################
 
 print("== Câu 5 ==")
 for i in range(1,n):
     print(f"{n} x {i} = {n * i}")
 
-
+############################
 
 print("== Câu 6 ==")
 for i in range(1, n + 1):
@@ -56,7 +58,7 @@ for i in range(1, n + 1):
         continue
     print(i)
 
-
+############################
 
 print("== Câu 8 ==")
 i=1
@@ -74,4 +76,22 @@ while(i<=n):
     except ValueError:
         print("Lỗi kiểu dữ liệu")
 print(f"Số nhỏ nhất: {min}")
-print(f"Số nhỏ nhất: {max}")
+print(f"Số lớn nhất: {max}")
+
+############################
+
+print("== Câu 9 ==")
+i=1
+dem = 0
+while(i<=10):
+    try:
+        so = int(input(f"Nhập số thứ {i}: "))
+        i += 1
+        if (so>0):
+            dem += 1
+    except ValueError:
+        print("Lỗi kiểu dữ liệu")
+print (f"Có {dem} số nguyên dương")
+
+############################
+
