0306c82b86bdd2e6d8ce80bd9d29a04697c22240
huong <huynhhuyenhuong2006@gmail.com>
2025-12-08 14:14:59 +0700

    update

---
+++ buoi6/baitap.py
@@ -0,0 +1,23 @@
+# print ("===Câu 1===")
+# s = str(input("Nhập: "))
+# print ("Chiều dài chuỗi là: ",len(s))
+
+# print ("===Câu 3===")
+# kitudau = s[:3]
+# kitucuoi = s[3:]
+# print (f"3 kí tự đầu tiên là: {kitudau}")
+# print (f"3 kí tự cuối là: {kitucuoi}")
+
+# print ("===Câu 4===")
+# print(f"In ra dạng viết hoa là: {s.upper()}")
+# print(f"In ra dạng viết thường là: {s.lower()}")
+
+
+# print ("===Câu 5===")
+# ho = str(input("Nhập họ: "))
+# tendem = str(input("Nhập tên đệm: "))
+# ten = str(input("Nhập tên: "))
+# hovaten = "Họ và tên là: {}" 
+# print(hovaten.format(ho),(tendem), (ten)) 
+
+print ("===Câu 6===")
