d93bc2bd38fa31388c894df55557e95dc056ebd9
huong <huynhhuyenhuong2006@gmail.com>
2025-12-22 09:37:48 +0700

    update

--- test.py
+++ test.py
@@ -31,47 +31,72 @@
 # print(f"Tổng tiền phải trả (đã gồm phí môi trường): {tong:,.0f} đ")
 
 
-# import random
+import random
 
-# def san_kho_bau():
-#     diem = 0
+def san_kho_bau():
+    diem = 0
 
-#     for i in range(1, 7):
-#         print("=> Lượt", i)
+    for i in range(1, 7):
+        print("=> Lượt", i)
 
-#         if random.random() < 0.6:
-#             diemcong = random.randint(5, 30)
-#             diem += diemcong
-#             print(f"Tìm thấy kho báu, được cộng {diemcong} điểm")
+        if random.random() < 0.8:
+            diemcong = random.randint(5, 30)
+            diem += diemcong
+            print(f"Tìm thấy kho báu, được cộng {diemcong} điểm")
+        else:
+            diem -= 2
+            if diem < 0:
+                diem = 0
+            print("Không tìm thấy kho báu, bị trừ 2 điểm")
+
+        print(f"Tổng điểm: {diem}")
+
+        if diem >= 80:
+            print("KẾT LUẬN: Thắng (đạt >= 80 điểm)")
+            return diem
+
+    # Sau 6 lượt
+    if diem < 80:
+        print("KẾT LUẬN: Thua (hết 6 lượt mà chưa đạt 80 điểm)")
+        return diem
+    
+sankhobao = san_kho_bau()
+print(f"Điểm cuối cùng: {sankhobao}")
+
+
+
+# Diem = [7.5, 8.0, 6.0, 9.0, 5.5, 8.5, 7.0, 6.5]
+
+# def diemtrungbinh(Diem):
+#     tong = 0
+#     for value in Diem:
+#         tong += value
+#     tb = tong / len(Diem)
+#     return tb
+
+# tinhdiem = diemtrungbinh(Diem)
+# print(f"Điểm trung bình là: {tinhdiem}")
+
+# def Diemcaonhat(Diem):
+#     max = Diem[0]
+#     for value in Diem:
+#         if value > max:
+#             max = value
+#     return max
+# tinhdiem = Diemcaonhat(Diem)
+# print(f"Điểm cao nhất là: {tinhdiem}")
+
+
+# def Demsodiem(Diem):
+#     lon = 0
+#     nho = 0
+#     for value in Diem:
+#         if value >= 8:
+#             lon += 1
 #         else:
-#             diem -= 2
-#             if diem < 0:
-#                 diem = 0
-#             print("Không tìm thấy kho báu, bị trừ 2 điểm")
+#             nho += 1
+#     return lon,nho
 
-#         print(f"Tổng điểm: {diem}")
-
-#         if diem >= 80:
-#             print("KẾT LUẬN: Thắng (đạt >= 80 điểm)")
-#             return diem
-
-#     # Sau 6 lượt
-#     if diem < 80:
-#         print("KẾT LUẬN: Thua (hết 6 lượt mà chưa đạt 80 điểm)")
-
-# san_kho_bau()
-
-
-
-Diem = [7.5, 8.0, 6.0, 9.0, 5.5, 8.5, 7.0, 6.5]
-
-def diemtrungbinh(Diem):
-    tong = 0
-    for value in Diem:
-        tong += value
-    tb = tong / len(Diem)
-    return tb
-
-tinhdiem = diemtrungbinh(Diem)
-print(f"Điểm trung bình là: {tinhdiem}")
-
+# demsodiem = Demsodiem(Diem)
+# print (f"Điểm từ 8 là: {demsodiem[0]}")
+# print (f"Điểm nhỏ hơn 8 là: {demsodiem[1]}")
